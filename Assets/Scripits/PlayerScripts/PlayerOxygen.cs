1200b7f6d3707f536c7585c3d6794fe254b0954f
Excell669 <excell663@gmail.com>
2026-01-15 16:39:22 -0800

    NewprojectSettings

--- Assets/Scripits/PlayerScripts/PlayerOxygen.cs
+++ Assets/Scripits/PlayerScripts/PlayerOxygen.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class PlayerOxygen : MonoBehaviour
 {
-    float oxygenLevel = 100f;
+  
+    [SerializeField] float maxOxygen = 100f;
+    [SerializeField] float depletionRateOutside = 10f; // oxygen lost per second when not in zone
+    [SerializeField] float regenRateInside = 15f; // oxygen restored per second inside zone
+    [SerializeField] float suffocationDamagePerSecond = 20f; // optional, require
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
